d8200875b58523bdd87e958b43e886155f900548
Martin Mercer <martinmercer1@gmail.com>
2018-12-12 10:23:28 -0700

    the class files I have 2/3 of the way through the challenge--meant to commit more, but just didn't. ¯\_(ツ)_/¯

---
+++ lib/show.rb
@@ -0,0 +1,22 @@
+class Show
+
+  attr_reader :name,
+              :creator,
+              :characters,
+              :total_salary
+
+  def initialize(name, creator, characters = [])
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    @characters.sum do |character|
+      @character.salary
+    end
+
+  end
+
+
+end
